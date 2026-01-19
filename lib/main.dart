894e954d00bf65e0dbc5ba91cc643eb103766e66
Thomas <thomaschinazam@gmail.com>
2026-01-20 00:25:17 +0100

    First commit: Full Stack Portfolio App

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
-import 'project_model.dart'; // Make sure this import matches your file structure
+import 'project_model.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 void main() {
